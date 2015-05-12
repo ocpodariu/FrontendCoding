f3e749ca4b4c46386e92c00e3b2ae0e6782e9f2f
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-05-12 18:41:57 +0300

    Add Menu Controller and its associated template

--- angularhr/scripts/MenuController.js
+++ angularhr/scripts/MenuController.js
@@ -1,3 +1,12 @@
 /**
  * Created by Ovidiu on 5/12/2015.
  */
+
+hrApp.controller('MenuController', ['$scope', function($scope) {
+    $scope.demoActionList = [
+        {
+            label: 'OtherScope',
+            url: 'views/childscope.html'
+        }
+    ];
+}]);
