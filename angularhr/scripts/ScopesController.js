74d89a344d9b88d34376788dcc83b1bcfa3e8b03
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-05-12 18:40:29 +0300

    Add Scope Controller and its associated template

--- angularhr/scripts/ScopesController.js
+++ angularhr/scripts/ScopesController.js
@@ -1,3 +1,15 @@
 /**
  * Created by Ovidiu on 5/12/2015.
  */
+
+hrApp.controller('ScopesController', ['$scope', function($scope) {
+    $scope.title = 'Two Way Binding Demo';
+    $scope.childTemplate = 'templates/childscope.html';
+
+    $scope.resetFirstVariable = function() {
+        $scope.firstVariable = undefined;
+    };
+    $scope.setFirstVariable = function(value) {
+        $scope.firstVariable = value;
+    };
+}]);
